ed4984d006cac496c224af85d1b91a51643885c6
shayanafkane <shayanafkane@gmail.com>
2022-12-01 13:10:07 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+function App() {
+  // useEffect(() => {
+
+  //   axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=06eab33cd5f147c733290f1947a2f9c0").then(res => {
+  //     console.log(res)
+  //   })
+  //     .catch(err => console.log(err))
+  // }, [])
+
+  return (
+    <div className="App">
+      ss
+    </div>
+  );
+}
+
+export default App;
